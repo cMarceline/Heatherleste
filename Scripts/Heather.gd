5d269b30b8af1738d92d43fcc1ee6f9cc5fcf38d
Marcie Collins <marciecollins12@gmail.com>
2026-04-26 15:41:20 +1000

    brokie lol

--- Scripts/Heather.gd
+++ Scripts/Heather.gd
@@ -1,23 +1,64 @@
 extends RigidBody2D
 
+const gravity : float = 9.8
 const maxSpeed : float = 400
-const friction : float = 0.2 * 60
-const dashSpeed : float = 500
+const friction : float = 0.1 * 60
+const dashSpeed : float = 1000
+const aerialDrag := Vector2(0.999,0.98)
 
 @onready var dashTimer : Timer = $dashTimer
 
-@export var groundSpeed : Vector2
+var groundSpeed : Vector2 #groundSpeed.y acts as the bounce retention
+var airTrajectory : Vector2
+
 var onFloor : bool
+func _onFloorCheck(state: PhysicsDirectBodyState2D) :
+	for i in state.get_contact_count() :
+		var contact_norm = state.get_contact_local_normal(i)
+		print(state.get_contact_impulse(i))
+		if contact_norm.y == -1 :
+			return true
+	return false
+
+var onWall : bool
+func _onWallCheck(state: PhysicsDirectBodyState2D) :
+	for i in state.get_contact_count() :
+		var contact_norm = state.get_contact_local_normal(i)
+		if abs(contact_norm.x) == 1 :
+			return true
+	return false
+
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	for contacts in state.get_contact_count() :
-		print(state.get_contact_local_normal(contacts))
+	onFloor = _onFloorCheck(state)
+	onWall = _onWallCheck(state)
+	
+	if !dashTimer.is_stopped: 
+		pass
+	
+	if onFloor :
+		linear_velocity = groundSpeed + airSpeed + dashSpeed
+		#launchTrajectory.y = lerp()
 
-	linear_velocity.x = groundSpeed.x
-	pass
-
+	
+	print(onFloor)
+	
 func _physics_process(delta: float) -> void:
-	pass
-	groundSpeed.x = lerp(groundSpeed.x, float(maxSpeed * Global.inputVector.x), friction*delta)
+	if !dashTimer.is_stopped: 
+		pass
+
+	if Global.inputDash : 
+		linear_velocity = Global.inputVector.normalized() * dashSpeed
+
+	if !onFloor :
+		linear_velocity.y += gravity
+		linear_velocity *= aerialDrag
+
+	if onFloor :
+		groundSpeed.x = lerp(groundSpeed.x, float(maxSpeed * Global.inputVector.x), friction*delta)
+		linear_velocity.y += -400 * int(Global.inputJump)
+		
+	
+	
 	# Walking
 	
--- Scripts/global.gd
+++ Scripts/global.gd
@@ -3,5 +3,11 @@ extends Node
 var inputVector : Vector2 :
 	get: return Vector2(
 		Input.get_action_raw_strength("Right") - Input.get_action_raw_strength("Left"),
-		Input.get_action_raw_strength("Up") - Input.get_action_raw_strength("Down")
+		Input.get_action_raw_strength("Down") - Input.get_action_raw_strength("Up")
 	)
+
+var inputJump : bool : 
+	get : return Input.get_action_raw_strength("Jump")
+	
+var inputDash : bool :
+	get : return Input.is_action_just_pressed("Dash")
