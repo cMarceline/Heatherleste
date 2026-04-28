45b05e08e2fd01ae134d6a2499e062ae30ae1c55
Marcie Collins <marciecollins12@gmail.com>
2026-04-28 13:12:58 +1000

    heather :)

--- Scripts/Heather.gd
+++ Scripts/Heather.gd
@@ -1,22 +1,34 @@
 extends RigidBody2D
 
 const gravity : float = 9.8
-const maxSpeed : float = 400
+const aerialDrag := Vector2(0.999,0.98)
+const terminalVelocity := (gravity * aerialDrag.y) / (1 - aerialDrag.y) 
+signal groundContactVelocity(x)
+
+const groundSpeed : float = 400
 const friction : float = 0.1 * 60
 const dashSpeed : float = 1000
-const aerialDrag := Vector2(0.999,0.98)
 
+signal dashPressed
 @onready var dashTimer : Timer = $dashTimer
 
-var groundSpeed : Vector2 #groundSpeed.y acts as the bounce retention
-var airTrajectory : Vector2
+var groundVelocity : Vector2 # groundVelocity.y acts as the bounce retention
+
+func _ready() -> void:
+	groundContactVelocity.connect(_bounceCalculation)
+	dashPressed.connect(_bounceCalculations)
+	
 
 var onFloor : bool
 func _onFloorCheck(state: PhysicsDirectBodyState2D) :
 	for i in state.get_contact_count() :
 		var contact_norm = state.get_contact_local_normal(i)
-		print(state.get_contact_impulse(i))
 		if contact_norm.y == -1 :
+			# Announce if just landed with velocity of landing
+			var contact_impulse = state.get_contact_impulse(i)
+			if contact_impulse != Vector2.ZERO :
+				groundContactVelocity.emit(contact_impulse)
+			# Return True if contacting floor
 			return true
 	return false
 
@@ -28,36 +40,35 @@ func _onWallCheck(state: PhysicsDirectBodyState2D) :
 			return true
 	return false
 
+func _bounceCalculation(bounce : Vector2) : 
+	if -bounce.y > terminalVelocity : 
+		groundVelocity.y = -bounce.y
+
+func _dashCalc()  : 
+	var normalisedInputVector = Global.inputVector.normalized()
+	if Global.inputVector.x == linear_velocity.x / abs(linear_velocity.x) : 
+		normalisedInputVector.x += dashSpeed
+	else :
+		normalisedInputVector.x = dashSpeed
+	if Global.inputVector.y == linear_velocity.y / abs(linear_velocity.y) : 
+		normalisedInputVector.y += dashSpeed
+	else : 
+		normalisedInputVector.y = dashSpeed
+
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	onFloor = _onFloorCheck(state)
 	onWall = _onWallCheck(state)
-	
-	if !dashTimer.is_stopped: 
-		pass
-	
-	if onFloor :
-		linear_velocity = groundSpeed + airSpeed + dashSpeed
-		#launchTrajectory.y = lerp()
-
-	
-	print(onFloor)
-	
-func _physics_process(delta: float) -> void:
-	if !dashTimer.is_stopped: 
-		pass
-
-	if Global.inputDash : 
-		linear_velocity = Global.inputVector.normalized() * dashSpeed
-
 	if !onFloor :
 		linear_velocity.y += gravity
-		linear_velocity *= aerialDrag
-
+		linear_velocity.y *= aerialDrag.y
+	
+func _physics_process(delta: float) -> void:
 	if onFloor :
-		groundSpeed.x = lerp(groundSpeed.x, float(maxSpeed * Global.inputVector.x), friction*delta)
-		linear_velocity.y += -400 * int(Global.inputJump)
-		
+		# Write to the Grounded Momentum
+		groundVelocity.x = linear_velocity.x
+		#if linear_velocity.y > terminalVelocity :
+		linear_velocity.x = lerp(linear_velocity.x,groundSpeed*Global.inputVector.x,friction*delta)
 	
 	
 	# Walking
